069e8fbed7dc9788d6299e1e3970171c0a4617c3
Przemek <przemek-szczepaniak@tlen.pl>
2020-04-25 14:41:20 +0200

    app edited to functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 /* global gapi */
 
-import React, { Component } from "react";
+import React from "react";
 const API_KEY = "AIzaSyBv9CFoSRPpUK11uwbfZLtu9pGDh91Ugaw";
-let m = [];
-class App extends Component {
-  loadYoutubeApi() {
+
+const App = () => {
+  let [vids, updateVids] = React.useState([]);
+  let [gapiReady, updateGapiState] = React.useState(false);
+  let isPlaylistLoaded = false;
+  let pageToken = undefined;
+  const loadYoutubeApi = () => {
     const script = document.createElement("script");
     script.src = "https://apis.google.com/js/client.js";
-
     script.onload = () => {
       gapi.load("client", () => {
         gapi.client.setApiKey(API_KEY);
         gapi.client.load("youtube", "v3", () => {
-          this.setState({ gapiReady: true });
+          updateGapiState(true);
           console.log(
             gapi.client.youtube.channels.list({
               part: "snippet,contentDetails,statistics",
@@ -22,42 +25,43 @@ class App extends Component {
         });
       });
     };
-
     document.body.appendChild(script);
-  }
-
-  componentDidMount() {
-    this.loadYoutubeApi();
-  }
-  execute() {
+  };
+  const execute = () => {
+    console.log("click");
     return gapi.client.youtube.playlistItems
       .list({
         part: "snippet,contentDetails",
         maxResults: 50,
         playlistId: "PLkcsM8kKgr7Z6xpqX78ZgzEip4l96Sbux",
-        pageToken: "CPQDEAA",
+        pageToken: pageToken,
       })
       .then(
         function (response) {
-          // Handle the results here (response.result has the parsed body).
-          // console.log("Response", response);
-          response.result.items.map((e) => m.push(e));
-          console.log(m);
+          console.log("response");
+          let vidsArr = vids;
+          !isPlaylistLoaded && response.result.items.map((e) => vidsArr.push(e));
+          updateVids(vidsArr);
+          if (response.result.nextPageToken) {
+            pageToken = response.result.nextPageToken;
+            execute();
+          } else isPlaylistLoaded = true;
         },
         function (err) {
           console.error("Execute error", err);
         }
       );
-  }
-  render() {
-    // if (this.state.gapiReady) {
-    return (
-      <div>
-        <button onClick={this.execute}>exec</button>
-      </div>
-    );
-    // }
-  }
-}
+  };
+  React.useEffect(() => {
+    loadYoutubeApi();
+  });
+  return gapiReady ? (
+    <div>
+      <button onClick={execute}>exec</button>
+    </div>
+  ) : (
+    <div></div>
+  );
+};
 
 export default App;
