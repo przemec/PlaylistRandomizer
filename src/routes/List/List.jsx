a07dbd877e4a8820c57ac4611f5dd5d43c5916a5
Przemek <przemek-szczepaniak@tlen.pl>
2020-06-06 15:43:41 +0200

    List.jsx now uses redux connect()

--- src/routes/List/List.jsx
+++ src/routes/List/List.jsx
@@ -3,47 +3,62 @@ import { gapi } from "gapi-script";
 import ResultsScreen from "../../modules/ResultsScreen";
 import LoadingPanel from "../../components/LoadingPanel";
 import Modal from "../../modules/Modal";
-import { loadPlaylist, slicePlaylist, randomizePlaylist, switchModal } from "../../store/actions";
-import { store } from "../../store";
+import * as A from "../../store/actions";
+import { connect } from "react-redux";
 
-const List = ({ match }) => {
+const List = ({ match, sliceP, randomizeP, loadP, switchM }) => {
   const [playlistLoaded, updatePLState] = React.useState(false);
   const [loadingErr, loadErr] = React.useState(false);
-  let pageToken = undefined;
   React.useEffect(() => {
+    let pageToken = undefined;
+    const search = (id) => {
+      gapi.client.youtube.playlistItems
+        .list({
+          part: "snippet,contentDetails",
+          maxResults: 50,
+          playlistId: id,
+          pageToken: pageToken,
+        })
+        .then(
+          function (response) {
+            loadP(response.result.items);
+            if (response.result.nextPageToken) {
+              pageToken = response.result.nextPageToken;
+              search(id);
+            } else {
+              sliceP();
+              randomizeP();
+              updatePLState(true);
+              switchM(false);
+            }
+          },
+          function (err) {
+            if (err.result.error.code === (404 || 403)) {
+              loadErr(true);
+            }
+          }
+        );
+    };
     match.params.id && !playlistLoaded && search(match.params.id);
-    store.dispatch(switchModal(true));
+    switchM(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const search = (id) => {
-    gapi.client.youtube.playlistItems
-      .list({
-        part: "snippet,contentDetails",
-        maxResults: 50,
-        playlistId: id,
-        pageToken: pageToken,
-      })
-      .then(
-        function (response) {
-          store.dispatch(loadPlaylist(response.result.items));
-          if (response.result.nextPageToken) {
-            pageToken = response.result.nextPageToken;
-            search(id);
-          } else {
-            store.dispatch(slicePlaylist());
-            store.dispatch(randomizePlaylist());
-            store.dispatch(switchModal(false));
-            updatePLState(true);
-          }
-        },
-        function (err) {
-          if (err.result.error.code === (404 || 403)) {
-            loadErr(true);
-          }
-        }
-      );
-  };
   return playlistLoaded ? <ResultsScreen /> : <Modal component={<LoadingPanel err={loadingErr} />} />;
 };
 
-export default List;
+const mapDTP = (dispatch) => ({
+  switchM: (e) => {
+    dispatch(A.switchModal(e));
+  },
+  randomizeP: () => {
+    dispatch(A.randomizePlaylist());
+  },
+  sliceP: () => {
+    dispatch(A.slicePlaylist());
+  },
+  loadP: (e) => {
+    dispatch(A.loadPlaylist(e));
+  },
+});
+
+export default connect(null, mapDTP)(List);
