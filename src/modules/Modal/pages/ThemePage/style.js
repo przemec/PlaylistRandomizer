d85b903d99c34358688036b4393612d0c0565d48
Przemek <przemek-szczepaniak@tlen.pl>
2020-06-17 00:54:00 +0200

    themed webkit-scrollbar added to theme settings

--- src/modules/Modal/pages/ThemePage/style.js
+++ src/modules/Modal/pages/ThemePage/style.js
@@ -3,4 +3,13 @@ import { Grid } from "@material-ui/core/";
 
 export const ThemesWrapper = styled(Grid)`
   overflow-y: scroll;
+  &::-webkit-scrollbar-track,
+  &::-webkit-scrollbar {
+    width: 8px;
+    border-radius: 2px;
+    background-color: ${({ theme }) => theme.backgroundAccent};
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.primary};
+  }
 `;
