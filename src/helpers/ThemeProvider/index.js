4753a0a893f6ea1daea9fa5f88e3b40a2a7baeb9
Przemek <przemek-szczepaniak@tlen.pl>
2020-06-19 00:11:50 +0200

    clg cleanup

--- src/helpers/ThemeProvider/index.js
+++ src/helpers/ThemeProvider/index.js
@@ -3,10 +3,9 @@ import themes from "../../assets/themes";
 import { connect } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
-const ThmProvider = ({ themeKey, themeType, children }) => {
-  console.log(themes, themes[themeType || "light"]);
-  return <ThemeProvider theme={themes[themeType || "light"][themeKey || "brick"]}>{children}</ThemeProvider>;
-};
+const ThmProvider = ({ themeKey, themeType, children }) => (
+  <ThemeProvider theme={themes[themeType || "light"][themeKey || "brick"]}>{children}</ThemeProvider>
+);
 
 const mapSTP = (store) => ({
   themeKey: store.theme.key,
